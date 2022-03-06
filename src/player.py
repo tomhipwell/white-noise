0449f88eff27da26ebb2fc05d1b505ac075ca361
Tom Hipwell <tom.j.hipwell@googlemail.com>
2022-03-06 20:05:10 +0000

    Initial commit.

---
+++ src/player.py
@@ -0,0 +1,17 @@
+"""Plays white noise indefinitely. Requires an mp3 in the same directory as the script."""
+
+import pygame
+
+
+def run():
+    """Loads mp3 and runs in infinite loop."""
+    pygame.mixer.init()
+    pygame.mixer.music.load("shhh.mp3")
+    pygame.mixer.music.play(-1)
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+
+if __name__ == "__main__":
+    run()
